25ccc6c713067791460b4ef8a48fabe06bf1731f
Ollie <38867365+OllieSteam@users.noreply.github.com>
2022-02-14 02:13:13 +0000

    Removed - Python

--- src/python/script.py
+++
@@ -1,8 +0,0 @@
-import random
-
-for x in range(5):
-    print (round(random.uniform(0, 0.5), 5))
-    print (round(random.uniform(0.2,0.6), 5))
-    print (round(random.uniform(0.4, 0.7), 5))
-    print (round(random.uniform(0.6,1), 5))
-
